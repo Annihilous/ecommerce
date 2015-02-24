12318620f594c97bbcc5f014b33ab8e2939de2a4
Annihilous <benjaminjnorton@gmail.com>
2015-02-24 17:33:29 -0500

    added pulldown options to navbar for brand and cat

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'storefront#all_items'
 
   get 'storefront/items_by_category'
 
   get 'storefront/items_by_brand'
+
+  get 'products' => 'products#index'
+  get 'categories' => 'categories#index'
+
+  get 'categorical' => 'storefront#items_by_category'
+
+  get 'branding' => 'storefront#items_by_brand'
 
   resources :categories
 
